7195b722222988b79395ab6106a36f6c205ec82d
kaen <kaencastszk@gmail.com>
2017-09-02 15:00:40 -0700

    add swimming+walking+leaping test

---
+++ src/test/java/org/terasology/flexiblemovement/SwimmingWalkingLeapingMovementTest.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2017 MovingBlocks
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.terasology.flexiblemovement;
+
+import org.junit.Test;
+
+public class SwimmingWalkingLeapingMovementTest extends FlexibleMovementTestingEnvironment {
+    @Test
+    public void simpleLeap() throws InterruptedException {
+        runTest(new String[]{
+                "~  |   ",
+                "~  |XXX"
+        }, new String[]{
+                "?  |   ",
+                "1  |23!"
+        });
+    }
+
+    @Test
+    public void threeDimensionalMoves() throws InterruptedException {
+        runTest(new String[]{
+                "~~~|~~ |   ",
+                "~ ~|~~~| XX",
+                "~~~| ~ | XX"
+        }, new String[]{
+                "?  |   |   ",
+                "   | 1 |   ",
+                "   |   |  !"
+        });
+    }
+
+    private void runTest(String[] world, String[] path) {
+        executeExample(world, path, "walking", "leaping", "swimming");
+    }
+}
